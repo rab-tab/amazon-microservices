5add85b9406aee3e0bea7b0dfaf32dbe240b211a
Rabia <sdetcode@gmail.com>
2026-09-01 19:01:03 +0530

    fix(redis): add explicit RedisTemplate config with StringRedisSerializer

---
+++ order-service/src/main/java/com/amazon/order/config/RedisConfig.java
@@ -0,0 +1,19 @@
+package com.amazon.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
